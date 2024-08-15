56ef76a55025d4e1125573dd9671125386bc5f7a
pavel.lysov <gerson90@mail.ru>
2024-08-15 21:42:29 +0200

    reformat code

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,16 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import tests.BaseTest;
 
-import java.io.File;
 
 public class SearchTest extends BaseTest {
     @Test(dataProvider = "dataProvider")
